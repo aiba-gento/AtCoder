5e27db5abf804fbadb38ab7ecb4af1d54dead0cf
gn10 <8gn24gn25@gmail.com>
2024-11-18 00:05:38 +0900

    難しいよ〜

---
+++ 20241117/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, k;
+    int m;
+    vector<int> Ai;
+    cin >> n >> k;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        }
+    return 0;
+}
